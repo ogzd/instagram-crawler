5e78f4efc1d2b2253b6073d5d32099dfdd6e3ff7
Orcun Simsek <osimsek@palantir.com>
2015-07-02 14:45:07 -0400

    small cleanup

--- crawler.py
+++ crawler.py
@@ -7,28 +7,9 @@ from instagram_api import InstagramApi
 
 
 ACCESS_TOKEN = open("access_token.txt").read()
+api = InstagramApi(ACCESS_TOKEN)
 
-# def add_female_friends(bag, api, user_id):
-# 	bag.insert(get_female_friends(api, user_id))
-
-# def get_female_friends(api, user_id):
-# 	# fill bag
-# 	user_ids = api.get_accessable_user_ids(user_id)
-# 	# sample to 100 friends due to request limitations
-# 	user_ids = random.sample(user_ids, min(len(user_ids), 100))
-# 	female_ids = set()
-# 	for user_id in user_ids:
-# 		username, fullname, bio = api.get_username_bio(user_id)
-# 		first_name = get_first_name(fullname)
-# 		if first_name == None: continue
-# 		if gender_detector.guess(first_name) == 'female':
-# 			female_ids.add(user_id)
-# 			if bio.lower().find("kik") != -1 or bio.lower().find("snap") != -1:
-# 				print username, bio
-# 	return female_ids
-
-def search_by_me():
-	api = InstagramApi(ACCESS_TOKEN)
+def search_by_me():	
 	bag = InstagramUserBag(InstagramUser(api = api, 
 										 user_name = api.data['username'],
 										 profile_picture = api.data['profile_picture'],
