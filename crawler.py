3e361bfcceb712a37705658749f1f30db90f2383
Orcun Simsek <osimsek@palantir.com>
2015-07-02 04:09:30 -0400

    insert only female friends (abazalik FTW!)

--- crawler.py
+++ crawler.py
@@ -21,31 +21,48 @@ def search(api, bag):
 		user_id = bag.pick_random()
 		bag.remove(user_id)
 
-		username, fullname, bio = api.get_username_bio(user_id)
-		if fullname: fullname = fullname.strip()
-
-		# hack on name
-		if not fullname or fullname == None or fullname == '': continue
-		if len(fullname.split(' ')) == 1: pass
-		if len(fullname.split(' ')) == 2: fullname = fullname.split(' ')[0]
-		if len(fullname.split(' ')) == 3: fullname = fullname.split(' ')[0]
-		if len(fullname.split(' ')) > 3:  fullname = ''.join(fullname.split(' '))
-		if not re.match("^[A-Za-z]*$", fullname): continue 
-
+		username, fullname, bio = api.get_username_bio(user_id)	
+		first_name = get_first_name(fullname)
+		print(first_name)
+		if first_name == None: continue
+		
 		# detect gender
-		gender = gender_detector.guess(fullname)
+		gender = gender_detector.guess(first_name)
 
 		if gender == 'female':
-			if bio.lower().find("kik") != -1:
+			if bio.lower().find("kik") != -1 or bio.lower().find("snap") != -1:
 				print username, bio
 		
 		# fill bag
-		user_ids = api.get_accessable_user_ids(user_id)
-		bag.insert(user_ids)
+		bag.insert(get_female_friends(api, user_id))
+
+def get_female_friends(api, user_id):
+	# fill bag
+	user_ids = api.get_accessable_user_ids(user_id)
+	female_ids = set()
+	for user_id in user_ids:
+		username, fullname, bioo = api.get_username_bio(user_id)
+		first_name = get_first_name(fullname)
+		if first_name == None: continue
+		if gender_detector.guess(first_name) == 'female':
+			female_ids.add(user_id)
+	return female_ids
+
+def get_first_name(fullname):
+	# hack on name
+	if not fullname or fullname == None: return None
+	fullname = fullname.strip()
+	if fullname == '': return None
+	if len(fullname.split(' ')) == 1: return None
+	if len(fullname.split(' ')) == 2: fullname = fullname.split(' ')[0]
+	if len(fullname.split(' ')) == 3: fullname = fullname.split(' ')[0]
+	if len(fullname.split(' ')) > 3:  fullname = ''.join(fullname.split(' '))
+	if not re.match("^[A-Za-z]*$", fullname): return None
+	return fullname; 
 
 def search_by_me():
 	api = InstagramApi(ACCESS_TOKEN)
-	bag = UserIdBag(api.get_accessable_user_ids(api.my_user_id))
+	bag = UserIdBag(get_female_friends(api, api.my_user_id))
 	search(api, bag)
 
 def search_by_query(query):
